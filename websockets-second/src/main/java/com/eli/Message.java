0345197255fceaea33c0041bad07988d557aaab1
antonstarasotnikau <anton.starasotnikau@gmail.com>
2022-06-16 16:27:43 +0300

    Websocket commit

---
+++ websockets-second/src/main/java/com/eli/Message.java
@@ -0,0 +1,17 @@
+package com.eli;
+
+public class Message {
+
+	private String from;
+	private String text;
+
+	public String getText() {
+
+		return text;
+	}
+
+	public String getFrom() {
+
+		return from;
+	}
+}
